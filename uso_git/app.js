bb13b0fd16b4053883c13c710905dd260deeecdd
INGYRMR <yayo0804.ymr@gmail.com>
2024-08-22 23:09:57 -0600

    Esto me lo enseño el porfesor Aaron

---
+++ uso_git/app.js
@@ -0,0 +1,18 @@
+let edad = prompt("Ingresa tu edad miamor: ");
+let nombre = prompt("Dame tu nombre miamor");
+let nacionalidad = prompt ("Dame tu nacionalidad miamor");
+
+console.log("Tu nombres es: "+ nombre);
+console.log("Tu nacionalidad es: "+ nacionalidad);
+
+if (parseInt(edad) >= 18 ){
+
+    console.log("Ya ponte a chambear");
+    console.log("Tu edad es de: "+edad+" años");
+
+}else{
+
+    console.log("Aún estas tierno");
+    console.log("Tu edad es de: "+edad+" años");
+
+}
