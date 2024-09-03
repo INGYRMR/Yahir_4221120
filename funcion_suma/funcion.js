f3a09b2216a52df1df868ca2412a5ffe7d053cff
INGYRMR <yayo0804.ymr@gmail.com>
2024-09-02 22:30:38 -0600

    Funcion suma

--- funcion_suma/funcion.js
+++ funcion_suma/funcion.js
@@ -1,5 +1,5 @@
-let a = prompt("Ingresa un primer valor a sumar: ")
-let b = prompt("Ingresa un segundo valor a sumar: ")
+let a =  parseInt(prompt("Ingresa un primer valor a sumar: "))    
+let b =  parseInt(prompt("Ingresa un segundo valor a sumar: "))    
 
 function sumar (a,b){
     return parseInt(a) + parseInt(b)
